45437f67887e3df7b10c998434db3ae2173c3b3b
codewithjalloh <codewithjalloh@gmail.com>
2024-05-29 16:45:23 +0100

    postMessage added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,23 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+
+  // firestore access
+  final FirebaseFirestore database = FirebaseFirestore();
+
+  // text controler
+  final TextEditingController newPostController = TextEditingController();
+
+  // post message functions
+  void postMessage() {
+    // only post message if there is something in the textfield
+    if (newPostController.text.isNotEmpty) {
+      String message = newPostController.text;
+      database.addPost(message);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
