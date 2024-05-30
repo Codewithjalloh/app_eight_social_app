ed7ae97e41f186e65e98df3143c0017ccb83155e
codewithjalloh <codewithjalloh@gmail.com>
2024-05-30 22:32:12 +0100

    drawer home added

--- lib/components /my_drawer.dart
+++ lib/components /my_drawer.dart
@@ -24,6 +24,22 @@ class MyDrawer extends StatelessWidget {
                   color: Theme.of(context).colorScheme.secondary,
                 ),
               ),
+
+              // home tile
+              Padding(
+                padding: EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.home,
+                    color: Theme.of(context).colorScheme.secondary,
+                  ),
+                  title: Text("H O M E"),
+                  onTap: () {
+                    // this already the home screen so just pop drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
             ],
           )
         ],
