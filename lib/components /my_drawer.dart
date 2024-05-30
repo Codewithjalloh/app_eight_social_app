551df39baf01aae91657f1fc8b974ba9e8db9f9d
codewithjalloh <codewithjalloh@gmail.com>
2024-05-30 18:27:56 +0100

    drawer added

---
+++ lib/components /my_drawer.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  void logout() {
+    FirebaseAuth.instance.signOut();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ();
+  }
+}
