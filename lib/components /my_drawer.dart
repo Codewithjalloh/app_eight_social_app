d9e74708f4be192d5e9f900003a9bd250db99d39
codewithjalloh <codewithjalloh@gmail.com>
2024-05-30 22:29:19 +0100

    drawer added

--- lib/components /my_drawer.dart
+++ lib/components /my_drawer.dart
@@ -10,6 +10,24 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ();
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              // drawer header
+              DrawerHeader(
+                child: Icon(
+                  Icons.favorite,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
