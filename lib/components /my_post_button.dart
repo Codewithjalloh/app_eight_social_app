293cb1fc7f3e700fa2d96e4ac84bd72b92ee8d21
codewithjalloh <codewithjalloh@gmail.com>
2024-06-01 12:54:56 +0100

    my post button complete

--- lib/components /my_post_button.dart
+++ lib/components /my_post_button.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
 
 class PostButton extends StatelessWidget {
-  PostButton({super.key});
+  final void Function()? onTap;
+
+  PostButton({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.secondary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        padding: EdgeInsets.all(16),
+        margin: EdgeInsets.only(left: 10),
+        child: Center(
+          child: Icon(
+            Icons.done,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+        ),
+      ),
+    );
   }
 }
