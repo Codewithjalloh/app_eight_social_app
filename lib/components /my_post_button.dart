06b820cb3960d45460e90b248e29a0e0f6e70196
codewithjalloh <codewithjalloh@gmail.com>
2024-06-01 12:52:04 +0100

    post button added

---
+++ lib/components /my_post_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PostButton extends StatelessWidget {
+  PostButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
