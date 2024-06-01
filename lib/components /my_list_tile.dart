51f7bec9148d48431d72c167f43a319bb5e821e5
codewithjalloh <codewithjalloh@gmail.com>
2024-06-01 12:46:53 +0100

    MyListTile added

---
+++ lib/components /my_list_tile.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dar';
+
+class MyListTile extends StatelessWidget {
+  final String title;
+  final String subTitle;
+  MyListTile({super.key, required this.title, required this.subTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
