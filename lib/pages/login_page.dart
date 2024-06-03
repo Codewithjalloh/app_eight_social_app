ef52f997df457c096bb7198e39f49d90bec36f71
codewithjalloh <codewithjalloh@gmail.com>
2024-06-03 16:54:30 +0100

    User and password controller login page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+class LoginPage extends StatefulWidget {
+  final void Function()? onTap;
+
+  LoginPage({super.key, required this.onTap});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  // User and password controller
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
