f87cc2777baa4e0ce42ba4a4ef4fc09c9ad0e5fe
codewithjalloh <codewithjalloh@gmail.com>
2024-06-03 17:00:44 +0100

    Scaffold added login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -40,6 +40,17 @@ class _LoginPageState extends State<LoginPage> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [],
+          ),
+        ),
+      ),
+    );
   }
 }
