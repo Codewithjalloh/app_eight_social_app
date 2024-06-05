553bc5c7173c32298221d6f34f3684e125fa203b
codewithjalloh <codewithjalloh@gmail.com>
2024-06-05 11:53:15 +0100

    // current logged in user

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
+
+  // current logged in user
+  final User? currentUser = FirebaseAuth.instance.currentUser;
 
   @override
   Widget build(BuildContext context) {
