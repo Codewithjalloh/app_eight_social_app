6cb4455c0533f9daf6e4c133e07477992df5c0f1
codewithjalloh <codewithjalloh@gmail.com>
2024-06-05 14:27:47 +0100

    expanded listview added

--- lib/pages/user_page.dart
+++ lib/pages/user_page.dart
@@ -1,3 +1,6 @@
+import 'package:app_eight_social_app/components%20/my_back_button.dart';
+import 'package:app_eight_social_app/helper/helper_funtion.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class UserPage extends StatelessWidget {
@@ -5,6 +8,48 @@ class UserPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection("Users").snapshots(),
+        builder: (context, snapshot) {
+          // error
+          if (snapshot.hasError) {
+            displayMessageToUser("Something went wrong", context);
+          }
+          // show loading circle
+          if (snapshot.data == null) {
+            return Text("No Data");
+          }
+          // get all users
+          final users = snapshot.data!.docs;
+
+          return Column(
+            children: [
+              // back button
+              Padding(padding: EdgeInsets.only(
+                top: 50.0,
+                left: 25.0,
+              ),
+              child: Row(
+                children: [
+                  MyBackButton(),
+                ],
+              ),
+              ),
+              Expanded(child: ListView.builder(
+                  itemCount: users.length,
+                  itemBuilder: (context, index) {
+                    // get individual user
+                    final user = users[index];
+
+
+
+              }))
+            ],
+          )
+        },
+      ),
+    );
   }
 }
