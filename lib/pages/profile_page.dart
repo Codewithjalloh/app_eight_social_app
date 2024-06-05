a00be8173c0c1bc3cf2a0b885611ca77d80cfad3
codewithjalloh <codewithjalloh@gmail.com>
2024-06-05 13:52:26 +0100

    extract data

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,3 +1,4 @@
+import 'package:app_eight_social_app/components%20/my_back_button.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -39,6 +40,26 @@ class ProfilePage extends StatelessWidget {
             // extract data
             Map<String, dynamic>? user = snapshot.data!.data();
             return Center(
+              child: Column(
+                children: [
+                  Padding(padding: EdgeInsets.only(top: 50.0, left: 25.0), child: Row(
+                    children: [
+                      MyBackButton(),
+                    ],
+                  ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+
+                //   Profile pic
+                  Container(
+                    decoration: BoxDecoration,
+                  )
+
+
+                ],
+              ),
 
             )
           }
