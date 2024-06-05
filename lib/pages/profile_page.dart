2229f330ef2fc37cc7b65e2a903c3d23e5a5907e
codewithjalloh <codewithjalloh@gmail.com>
2024-06-05 11:55:39 +0100

    future for user details

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,11 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
 
   // current logged in user
   final User? currentUser = FirebaseAuth.instance.currentUser;
+
+  // future for user details
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserDetails() async {
+    return await FirebaseFirestore.instance
+        .collection("Users")
+        .doc(currentUser!.email)
+        .get();
+  }
 
   @override
   Widget build(BuildContext context) {
