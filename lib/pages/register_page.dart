7cb2bc4a8c6d00eebe06f2d54e0a45aeffd43de2
codewithjalloh <codewithjalloh@gmail.com>
2024-06-06 16:51:10 +0100

    User and password controller

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -9,7 +9,11 @@ class RegisterPage extends StatefulWidget {
 
 class _RegisterPageState extends State<RegisterPage> {
   // User and password controller
-
+  final TextEditingController usernameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
