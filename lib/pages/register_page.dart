b9d6a2ba075093c6a3b1d1ee415e261208263f12
codewithjalloh <codewithjalloh@gmail.com>
2024-06-06 16:49:38 +0100

    RegisterPage added

---
+++ lib/pages/register_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  // User and password controller
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
