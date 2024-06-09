178124a8a5dc2cc215be0a93af3006394e2d517a
codewithjalloh <codewithjalloh@gmail.com>
2024-06-09 13:47:10 +0100

    darkmode comlete

--- lib/theme/light_mode.dart
+++ lib/theme/light_mode.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
-    brightness: Brightness.light,
-    colorScheme: ColorScheme.light(
-      surface: Colors.grey.shade300,
-      primary: Colors.grey.shade200,
-      secondary: Colors.grey.shade800,
-    ),
-    textTheme: ThemeData.light().textTheme.apply(
-          bodyColor: Colors.grey[800],
-          displayColor: Colors.black,
-        ));
+  brightness: Brightness.light,
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade300,
+    primary: Colors.grey.shade200,
+    secondary: Colors.grey.shade800,
+  ),
+  textTheme: ThemeData.light().textTheme.apply(
+        bodyColor: Colors.grey[800],
+        displayColor: Colors.black,
+      ),
+);
