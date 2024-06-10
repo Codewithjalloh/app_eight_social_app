f75f73c81320fb9ce93e37267637cac9d6da9861
codewithjalloh <codewithjalloh@gmail.com>
2024-06-10 10:36:00 +0100

    my drawer complete

--- lib/components /my_drawer.dart
+++ lib/components /my_drawer.dart
@@ -43,7 +43,7 @@ class MyDrawer extends StatelessWidget {
                 padding: const EdgeInsets.only(left: 25.0),
                 child: ListTile(
                   leading: Icon(
-                    Icons.home,
+                    Icons.person,
                     color: Theme.of(context).colorScheme.secondary,
                   ),
                   title: const Text("P R O F I L E"),
@@ -62,7 +62,7 @@ class MyDrawer extends StatelessWidget {
                 padding: const EdgeInsets.only(left: 25.0),
                 child: ListTile(
                   leading: Icon(
-                    Icons.home,
+                    Icons.group,
                     color: Theme.of(context).colorScheme.secondary,
                   ),
                   title: const Text("U S E R"),
